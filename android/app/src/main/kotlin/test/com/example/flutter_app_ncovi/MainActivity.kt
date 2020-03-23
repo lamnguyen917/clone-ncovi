07dbe9dd07b7da1a23e2b7d94785b12fca24f5ad
quangthu1162 <thunq@rikkeisoft.com>
2020-03-23 09:45:28 +0700

    create new project

---
+++ android/app/src/main/kotlin/test/com/example/flutter_app_ncovi/MainActivity.kt
@@ -0,0 +1,13 @@
+package test.com.example.flutter_app_ncovi
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+}
